23c7bb3bff70c3468e672ea152e4457e165ca604
laurasanchezcardoso <lausanchezcardoso@gmail.com>
2020-01-23 11:46:12 -0300

    test key

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -67,6 +67,7 @@ class BlogsController < ApplicationController
       @blog = Blog.find(params[:id])
     end
 
+    #test key
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body)
